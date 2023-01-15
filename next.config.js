48729f1e8ea183727212078e7220a46965f739f5
Your Name <you@example.com>
2023-01-15 18:50:10 +0500

    commit 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
+  env: {
+    ABI: process.env.ABI,
+    CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
